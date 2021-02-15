cd2c86beff2cd6346fb6f900aaea3a09a98af483
mary9026 <mary9026@naver.com>
2021-02-15 10:11:49 +0900

    팀원의 게시판 기능 구현

---
+++ src/main/java/mary/spring/mvc/controller/BoardController.java
@@ -0,0 +1,13 @@
+package mary.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("board/list")
+    public String list() {
+        return "board/list";
+    }
+}
