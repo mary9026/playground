86876824fd2ab2139bb35b2e7eaf1ffc45cb568d
mary9026 <mary9026@naver.com>
2021-02-15 10:52:14 +0900

    팀원3의 갤러리 기능 구현

---
+++ src/main/java/mary/spring/mvc/controller/GalleryController.java
@@ -0,0 +1,13 @@
+package mary.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GalleryController {
+
+    @GetMapping("gallery/list")
+    public String list() {
+        return "gallery/list";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/gallery/list.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Gallery - list</title>
+</head>
+<body>
+<h1>Gallery - list</h1>
+</body>
+</html>
